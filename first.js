8296172c44cda7d356683142d4e838bfb69394b6
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-09 09:37:39 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,41 @@
+console.log("hello")
+
+console.log(2+2)
+
+let num = 5+5
+console.log(num)
+
+num = 5+5
+console.log(num)
+
+console.log(num + 5)
+
+let user = 'skr'
+console.log(user)
+
+let r = 5
+const pi = 3.14
+let area
+
+r = 6
+area = pi * r * r
+console.log(area)
+
+let num1 = 7.8
+console.log(typeof num1)
+
+let num2 = 1984298984896949889n
+console.log(num2)
+console.log(num2 + 2n)
+
+
+
+
+let num3 = 4
+
+let x = ++num3
+console.log(x)
+
+let y = x+1
+console.log(y)
+console.log(num3)
