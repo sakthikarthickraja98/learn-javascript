7812edc869d63b1395fb95ef42f467480117bace
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-16 10:38:01 +0530

    fun in obejct

--- second.js
+++ second.js
@@ -6,3 +6,18 @@ let twonum = (num10, num11) => {
 let ans = twonum(230,270)
 
 console.log(ans);
+
+
+//function(method) inside an object
+let laptop = {
+    cpu: 'i9',
+    ram: 16,
+    brand: 'HP',
+
+
+    greet: function(){
+        console.log('Hello world');
+    }
+}
+
+laptop.greet();
